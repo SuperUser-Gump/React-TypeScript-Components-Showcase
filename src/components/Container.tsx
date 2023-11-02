bb46ca50a806ccc5db10d8671144ee7f6f225d90
Krittin Saenpakdi <gump29062@gmail.com>
2023-11-03 03:36:18 +0700

    feat: Enhance 'Container' component for element type customization
    - Enhanced the 'Container' component to provide flexibility in specifying the element type and rendering children.
    - The component now allows customization of the element type using the 'as' prop and renders the specified element or defaults to a 'div' element.

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,7 +5,7 @@ type ContainerProps<T extends ElementType> = {
   children: ReactNode;
 } & ComponentPropsWithoutRef<T>;
 
-export default function Container({ as }: ContainerProps) {
-  const Component = as;
-  return <Component />;
+export default function Container<C extends ElementType>({ as, children, ...props }: ContainerProps<C>) {
+  const Component = as || 'div';
+  return <Component {...props}>{children}</Component>;
 }
