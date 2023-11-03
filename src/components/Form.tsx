6e20da1222b98c528e16388404b7e3452b9849d6
Krittin Saenpakdi <gump29062@gmail.com>
2023-11-03 15:05:28 +0700

    feat: Add 'handleSubmit' function to 'Form' component
    - Added the 'handleSubmit' function in the 'Form' component, enabling form submission handling.
    - The function prevents the default form submission, extracts form data, and passes it to the 'onSave' callback for further processing.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -4,4 +4,18 @@ type FormProps = ComponentPropsWithoutRef<'form'> & {
   // eslint-disable-next-line no-unused-vars
   onSave: (value: unknown) => void;
 };
+export default function Form({ onSave, children, ...otherProps }: FormProps) {
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData);
+    onSave(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit} {...otherProps}>
+      {children}
+    </form>
+  );
 }
